e83b9e6f745a8fb1ab10a14422d9b4d8ccc48f5c
Bskasan <b.kasan@yandex.com>
2024-01-26 23:14:07 +0200

    up - game events

--- Assets/Scripts/Game Events/GameEvent.cs
+++ Assets/Scripts/Game Events/GameEvent.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Game Event")]
 public class GameEvent : ScriptableObject
 {
     private HashSet<GameEventListener> _gameEventListeners = new HashSet<GameEventListener> ();   
 
     public void Register(GameEventListener gameEventListener) => _gameEventListeners.Add(gameEventListener);
+    public void DeRegister(GameEventListener gameEventListener) => _gameEventListeners.Remove(gameEventListener);
+
+    public void Invoke()
+    {
+        foreach (GameEventListener gameEventListener in _gameEventListeners)
+            gameEventListener.RaiseEvent();
+    }
 
 
 }
--- Assets/Scripts/Game Events/GameEventListener.cs
+++ Assets/Scripts/Game Events/GameEventListener.cs
@@ -1,6 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class GameEventListener : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _unityEvent;
 
+    public void RaiseEvent()
+    {
+        _unityEvent.Invoke();
+    }
 }
