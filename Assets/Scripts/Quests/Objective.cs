a4bde01fe6125f180082f5c4cc33f47dfb8d72ee
Bskasan <b.kasan@yandex.com>
2024-01-31 19:54:24 +0200

    up - step and objective type scripts

--- Assets/Scripts/Quests/Objective.cs
+++ Assets/Scripts/Quests/Objective.cs
@@ -1,6 +1,15 @@
 ﻿using System;
+using UnityEngine;
 
 [Serializable]
 public class Objective
 {
+    [SerializeField] private ObjectiveType _objectiveType;
+
+    public enum ObjectiveType
+    { 
+        Flag,
+        Item,
+        Kill,
+    }
 }
--- Assets/Scripts/Quests/Step.cs
+++ Assets/Scripts/Quests/Step.cs
@@ -1,9 +1,11 @@
-﻿using Ink.Parsed;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 [Serializable]
 public class Step
 {
+    [SerializeField] private string _instructions;
+
     public List<Objective> Objectives;
 }
