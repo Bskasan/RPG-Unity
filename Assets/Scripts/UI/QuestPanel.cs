a5724ed42e2e6a8256a72773c9cb81b0b285bd0f
Bskasan <b.kasan@yandex.com>
2024-02-12 22:08:23 +0200

    up - created quest panel script

---
+++ Assets/Scripts/UI/QuestPanel.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestPanel : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private TMP_Text _descriptionText;
+    [SerializeField] private TMP_Text _currentObjectivesText;
+    [SerializeField] private Image _iconImage;
+}
